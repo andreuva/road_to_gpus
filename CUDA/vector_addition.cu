9ca4448014a98bc10a8cd3d1e35e4f46f067f47c
andreuva <andres.vicente.arevalo@gmail.com>
2021-03-02 09:39:33 +0000

    changes to debug

--- CUDA/vector_addition.cu
+++ CUDA/vector_addition.cu
@@ -3,10 +3,11 @@
 #include <math.h>
 
 __global__ void cuda_hello(){
-    printf("Hello World from GPU!\n");
+    fprintf(stdout,"Hello World from GPU!\n");
 }
 
 int main() {
+    fprintf(stdout,"Hello from the main program!!\n");
     cuda_hello<<<4,4>>>(); 
     return 0;
 }
