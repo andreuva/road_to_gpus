fd9c296599aaa6bd5bd38cd507f72048fc97f056
andreuva <andres.vicente.arevalo@gmail.com>
2021-03-04 09:16:08 +0000

    added device variables and copy to and from device

--- CUDA/libraries/eigenvalues_vectors.cu
+++ CUDA/libraries/eigenvalues_vectors.cu
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <cuda_runtime.h>
-#include "cusolverDn.h"
+#include <cusolverDn.h>
 
 
 void printMatrixArray(int m, int n, const double *A, int lda, const char* name){
@@ -35,17 +35,26 @@ void printMatrix(int m, int n, double **A, int lda, const char* name){
 
 
 int main(int argc, char **argv){
-    cusolverStatus_t cusolver_status;
+    cusolverStatus_t cusolver_status = CUSOLVER_STATUS_SUCCESS;
     cusolverDnHandle_t cusolver_handle = NULL;
-    cudaError_t cudaStat1 = cudaSuccess;
+    cudaError_t cudaStat = cudaSuccess;
+
+    cusolverEigMode_t jobz;
+    cublasFillMode_t uplo;
 
     int N, i, j;
-    double *A, **A_host;
+    double *A, **A_host, *Evectors, *Evalues;
+    double *device_A = NULL;
+    double *device_Eigenvalues = NULL;
+    int *device_info = NULL;
+    int info_gpu
 
     // memory allocation and parameter set up
     N = 5;
 
     A = (double *) malloc(N * N * sizeof(double));
+    Evectors = (double *) malloc(N * N * sizeof(double));
+    Evalues = (double *) malloc(N * sizeof(double));
     A_host = (double **) malloc(N * sizeof(double *));
     for (i = 0; i < N; i++) {
         A_host[i] = (double *) malloc(N * sizeof(double));
@@ -61,18 +70,64 @@ int main(int argc, char **argv){
     // copy content of A_host[][] into their linear version A[] --- column wise !
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
-            A[(i*N)+j] = A_host[j][i];
+            A[(i*N)+j] = A_host[i][j];
         }
     }
 
+    printf("================================================")
     printMatrix(N, N, A_host, N, "A_host");
     printMatrixArray(N, N, A, N, "A");
+    printf("================================================")
 
-    // initiate the cusolverDn context
-    cusolverDnCreate(&cusolver_handle);
+    jobz = CUSOLVER_EIG_MODE_VECTOR; // compute eigenvalues and eigenvectors
+    uplo = CUBLAS_FILL_MODE_LOWER; 
 
-    // close the cusolverDn context
-    cusolverDnDestroy(&cusolver_handle);
+    // Step 1: initiate the cusolverDn context
+    cusolver_status = cusolverDnCreate(&cusolver_handle);
+    assert(CUSOLVER_STATUS_SUCCESS == cusolver_status);
+
+    // step 2: copy matrices to device
+    cudaStat = cudaMalloc ((void**)&device_A, sizeof(double) * N * N);
+    assert(cudaSuccess == cudaStat);
+    cudaStat = cudaMalloc ((void**)&device_Eigenvalues, sizeof(double) * N);
+    assert(cudaSuccess == cudaStat);
+    cudaStat = cudaMalloc ((void**)&device_info, sizeof(int));
+    assert(cudaSuccess == cudaStat);
+    cudaStat = cudaMemcpy(device_A, A, sizeof(double) * N * N, cudaMemcpyHostToDevice);
+    assert(cudaSuccess == cudaStat);
+
+
+    // step 5: retrieve the results from device memory
+    cudaStat = cudaMemcpy(Evalues, device_Eigenvalues, sizeof(double)*m, cudaMemcpyDeviceToHost);
+    assert(cudaSuccess == cudaStat);
+    cudaStat = cudaMemcpy(Evectors, device_A, sizeof(double)*lda*m, cudaMemcpyDeviceToHost);
+    assert(cudaSuccess == cudaStat);
+    cudaStat = cudaMemcpy(&info_gpu, device_info, sizeof(int), cudaMemcpyDeviceToHost);
+    assert(cudaSuccess == cudaStat);
+
+    // step 6: print out results, ie eigenvalues and corresponding eigenvectors
+    printf(" Eigenvalues\n");
+    printMatrixArray(1, N, Evalues, 1, "lambda");
+    printf("================================================")
+    printf(" Corresponding Eigenvectors\n");
+    printMatrixArray(N, N, Evectors, N, "V");
+    printf("================================================")
+    printf("\n");
+    
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            printf("%10.2lf", Evectors[(j*m)+i]);
+        }
+        printf("\n");
+    }
+  
+    // step 7: free all allocated memory and destroy context
+   cudaFree(device_A);
+   cudaFree(device_Eigenvalues);
+   cudaFree(device_info);
+   
+   cusolverDnDestroy(cusolver_handle);
+   cudaDeviceReset();
 
     for (i = N-1; i >= 0; i--) {
         free(A_host[i]);
