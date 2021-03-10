cb350b8dd01b970c88b549df76a5e70aca1ea6c6
andreuva <andres.vicente.arevalo@gmail.com>
2021-03-10 16:23:15 +0000

    implemented dotprod test

--- dotprod/dotprod_cpu.c
+++ dotprod/dotprod_cpu.c
@@ -1,34 +1,16 @@
-/*
- * Copyright 1993-2015 NVIDIA Corporation.  All rights reserved.
- *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
- *
- */
+/***************************************************************
+        DOT PRODUCT FUNCTION IMPLEMENTED FOR THE CPU
+    AUTHOR: ANDRES VICENTE AREVALO      DATE:10/03/2021
+****************************************************************/
 
-////////////////////////////////////////////////////////////////////////////////
-// export C interface
-extern "C"
-void computeGold(float *reference, float *idata, const unsigned int len);
+// Function that compute the dot product between two arrays
+float dotprod(float *A, float *B, int len){
+    int i;
+    float result = 0;
 
-////////////////////////////////////////////////////////////////////////////////
-//! Compute reference data set
-//! Each element is multiplied with the number of threads / array length
-//! @param reference  reference data, computed but preallocated
-//! @param idata      input data as provided to device
-//! @param len        number of elements in reference / idata
-////////////////////////////////////////////////////////////////////////////////
-void
-computeGold(float *reference, float *idata, const unsigned int len)
-{
-    const float f_len = static_cast<float>(len);
-
-    for (unsigned int i = 0; i < len; ++i)
-    {
-        reference[i] = idata[i] * f_len;
+    for (i = 0; i < len; i++){
+        result += A[i] * B[i];
     }
-}
 
+    return result;
+}
